6249867b4327198655fe489e3be1694fcf99447c
Daksh Chawla <daksh22chawla@gmail.com>
2026-09-20 20:03:26 +0530

    Update API_BASE to handle different environments

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,6 +1,10 @@
 import { auth } from "./firebase-config.js";
 
-const API_BASE = "http://localhost:4000";
+const API_BASE =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+        ? "http://localhost:4000"
+        : "";
 
 
 // ===============================
