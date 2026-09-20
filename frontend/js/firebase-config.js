15a4c96297e594ad57736df32b8c085b88ab93e8
Daksh Chawla <daksh22chawla@gmail.com>
2026-09-20 20:04:49 +0530

    Update EcoTrack for Vercel deployment

--- frontend/js/firebase-config.js
+++ frontend/js/firebase-config.js
@@ -1,34 +1,32 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+const {
+    initializeApp,
+    cert,
+    getApps
+} = require("firebase-admin/app");
 
-import {
-    getAuth,
-    GoogleAuthProvider
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+const {
+    getAuth
+} = require("firebase-admin/auth");
 
+let serviceAccount;
 
-const firebaseConfig = {
+if (
+    process.env.FIREBASE_SERVICE_ACCOUNT_JSON
+) {
+    serviceAccount = JSON.parse(
+        process.env.FIREBASE_SERVICE_ACCOUNT_JSON
+    );
+} else {
+    serviceAccount =
+        require("./serviceAccountKey.json");
+}
 
-  apiKey: "AIzaSyA_pxE2yOiJ-W0VSbouYa4QEQdwb6c34BA",
-  authDomain: "ecotrack-bdc9c.firebaseapp.com",
-  projectId: "ecotrack-bdc9c",
-  storageBucket: "ecotrack-bdc9c.firebasestorage.app",
-  messagingSenderId: "402710844262",
-  appId: "1:402710844262:web:c3a39ae214b04387fe2ea7"
+if (getApps().length === 0) {
+    initializeApp({
+        credential: cert(serviceAccount)
+    });
+}
 
-};
+const auth = getAuth();
 
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-
-const googleProvider = new GoogleAuthProvider();
-
-
-export {
-
-    auth,
-
-    googleProvider
-
-};
+module.exports = auth;
